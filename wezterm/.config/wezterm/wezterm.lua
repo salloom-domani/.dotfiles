860a82d6c372e0b4dc54cd770e5fb50afa0d2d43
Salem Domani <salem.domani@gmail.com>
2023-11-01 07:50:08 +0300

    add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- Copy/Paste on mouse right click
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = act.PasteFrom("Clipboard"),
+	},
+}
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+
+-- default is true, has more "native" look
+config.use_fancy_tab_bar = false
+
+-- I don't like putting anything at the ege if I can help it.
+config.enable_scroll_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.tab_bar_at_bottom = true
+config.freetype_load_target = "HorizontalLcd"
+
+-- and finally, return the configuration to wezterm
+return config
