a122fb5ed3a07aec5bb03dddbaa28995d3cd74d5
Salem Domani <salem.domani@gmail.com>
2024-10-04 16:58:38 +0300

    feat: add docker service aliases

--- zsh/.config/zsh/include/aliases.zsh
+++ zsh/.config/zsh/include/aliases.zsh
@@ -4,3 +4,7 @@ alias la='ls -Al'
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
+
+# docker service
+alias start-docker='sudo systemctl start docker && echo "Docker started successfully"'
+alias stop-docker='sudo systemctl stop docker.socket && echo "Docker stopped successfully"'
